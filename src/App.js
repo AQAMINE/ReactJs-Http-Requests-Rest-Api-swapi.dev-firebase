68bb443c2f8b7a3925baac8602960d6b81459405
amine aqebli <aqebli36@gmail.com>
2022-09-15 02:33:26 +0100

    Async/await

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films').then( response => {
-      return response.json();
-    }).then( data => {
-      const trqnsformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
 
-        };
-      });
-      setMovies(trqnsformedMovies);
+    const trqnsformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+
+      };
     });
+    setMovies(trqnsformedMovies);
+    
   }
 
   return (
